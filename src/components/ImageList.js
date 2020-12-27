c5de06152b4bd83826f32986a0c85c189bfc5612
volkanb <volkanbenli92@gmail.com>
2020-12-26 19:40:17 -0500

    Adds ImageCard component into ImageList component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,10 @@
 import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
-	const images = props.images.map(({ id, description, urls }) => {
-		return <img src={urls.regular} key={id} alt={description} />;
+	const images = props.images.map((image) => {
+		return <ImageCard key={image.id} image={image} />;
 	});
 
 	return <div className='image-list'>{images}</div>;
