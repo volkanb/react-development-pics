c26fe6c2b89e2381013c07ef72c1b6a0adcb7779
volkanb <volkanbenli92@gmail.com>
2020-12-26 19:08:09 -0500

    Adds alt and key props to Image

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const ImageList = (props) => {
-	console.log(props.images);
+	const images = props.images.map(({ id, description, urls }) => {
+		return <img src={urls.regular} key={id} alt={description} />;
+	});
 
-	return <div>ImageList</div>;
+	return <div>{images}</div>;
 };
 
 export default ImageList;
